d92f6dad6d03e495c27de0279b3ea12709f0a4a0
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-09-15 19:04:02 +0100

    fix: temporary disable listus bot configuration due to deps issue

---
+++ listus/bot4listus/router.go
@@ -0,0 +1,13 @@
+package bot4listus
+
+//var ListusBotProfile botsfw.BotProfile
+
+func init() {
+	//commandsByType := map[botsfw.WebhookInputType][]botsfw.Command{
+	//	botsfw.WebhookInputText: listusBotCommands,
+	//}
+	//router := botsfw.NewWebhookRouter(commandsByType, func() string {
+	//	return "Please report any issues to @trakhimenok"
+	//})
+	//sneatbots.NewProfile("listus", &router)
+}
